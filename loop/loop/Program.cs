efbf1a2002c6691a5b5b382c8701540eaf5c5c82
nathanmotta <142407439+nathanmotta@users.noreply.github.com>
2024-03-12 18:57:30 -0300

    Add files via upload

---
+++ loop/loop/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaração de Variaveis
+            int t;
+
+            // Calculo da velocidade a cada instante
+            for (t = 1; t < 5; t++)
+            {
+                Console.WriteLine("Velocidade = " + (60 - 5 * t));
+            }
+
+            // Verificação do valor de t
+            Console.WriteLine(t);
+
+            // Aguarda a digitação de uma tecla para encerrar o programa
+            Console.ReadKey();
+        }
+    }
+}
